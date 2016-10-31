f52fec6a0283d96d724689f44d58d7f41745e178
stormand <422171566@qq.com>
2016-10-31 22:29:03 +0800

    test

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
 
   def index
+    @course=Course.all
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   include SessionsHelper
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember_user(user) : forget_user(user)
