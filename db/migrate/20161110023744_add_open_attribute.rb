5ced093e23630a2276465a5a20d0faf372fc118d
Zi Kangli <zikangli@126.com>
2016-11-10 13:27:11 +0000

    “课程开放关闭功能+选课计数”

---
+++ db/migrate/20161110023744_add_open_attribute.rb
@@ -0,0 +1,6 @@
+class AddOpenAttribute < ActiveRecord::Migration
+  def change
+    add_column :courses, :open, :boolean
+    change_column_default :courses, :open, from: true, to: false
+  end
+end
