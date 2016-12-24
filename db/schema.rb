9bf3844b9c54c237015ea125b911a2a8a33294f1
hydrogen <hydrogen@localhost.localdomain>
2016-12-24 12:58:47 +0800

    数据库修改[C

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215121614) do
+ActiveRecord::Schema.define(version: 20161224045044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,8 +66,9 @@ ActiveRecord::Schema.define(version: 20161215121614) do
     t.integer  "course_id"
     t.integer  "user_id"
     t.integer  "grade"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "apply",      default: false
   end
 
   add_index "grades", ["course_id"], name: "index_grades_on_course_id", using: :btree
