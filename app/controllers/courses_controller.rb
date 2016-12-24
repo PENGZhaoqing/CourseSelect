cf5aa30fd1227c71e5ea696a2c55fd6b69b472a4
Zi Kangli <zikangli@126.com>
2016-12-24 08:39:36 +0000

    背景div调整，表单显示内容添加“授课地点”；学分统计视图与函数功能分离；图表尝试绘制

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -91,6 +91,33 @@ class CoursesController < ApplicationController
   def credittips
     @courses=current_user.courses
     @grades=current_user.grades
+    @chosen_credit_all = 0.0
+    @chosen_credit_public = 0.0
+    @chosen_credit_major = 0.0
+    @courses.each do |course|
+      @chosen_credit_all = @chosen_credit_all+ course.credit[-3..-1].to_f
+      if course.course_type == "公共选修课" then
+         @chosen_credit_public = @chosen_credit_public+course.credit[-3..-1].to_f
+      end
+      if course.course_type == "专业核心课" then
+         @chosen_credit_major = @chosen_credit_major+course.credit[-3..-1].to_f
+      end
+    end
+    
+   @obtained_credit_pubic = 0.0
+   @obtained_credit_major = 0.0
+   @obtained_credit_all = 0.0
+   @grades.each do |grade|
+      if grade.grade.nil? == false
+         @obtained_credit_all += grade.course.credit[-3..-1].to_f
+         if grade.course.course_type == "公共选修课"
+            @obtained_credit_public += grade.course.credit[-3..-1].to_f
+         end
+         if grade.course.course_type == "专业核心课"
+             @obtained_credit_major += grade.course.credit[-3..-1].to_f
+         end
+      end
+   end
   end
   
  
