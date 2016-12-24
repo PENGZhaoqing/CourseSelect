24b820036943329e6369cbe294bd8aeebeb90f42
hydrogen <hydrogenlee@github.com>
2016-12-24 21:01:48 +0800

    gemfile

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Vim
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
